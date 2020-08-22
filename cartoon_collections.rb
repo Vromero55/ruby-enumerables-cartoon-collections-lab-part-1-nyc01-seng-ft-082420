809ebb9d433bc8a242323fc83d1739b505957584
Victor Romero <romeroa.victor@yahoo.com>
2020-08-22 19:22:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,9 +2,7 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  array.each do |element|
-    array[element] = "I said something as #{element}"
-    array
+  array.each {|element| element = "#{element} started"
 end
 
 def list_dwarves(array)
