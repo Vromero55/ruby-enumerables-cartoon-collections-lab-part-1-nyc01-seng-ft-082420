041d8598671277a14e9afe0aae2a49914c3b03ae
Victor Romero <romeroa.victor@yahoo.com>
2020-08-22 19:15:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |element|
-    puts "I made a greeting for #{element}"
+  element =   puts "I made a greeting for #{element}"
 end
 
 def list_dwarves(array)
